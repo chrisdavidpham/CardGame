28cd909e362bbca4c4ac53632a342a68648ab9bc
Pham <chrisdavidpham@gmail.com>
2019-10-11 16:54:04 -0500

    Better comments

--- CardGame/Player.cs
+++ CardGame/Player.cs
@@ -19,6 +19,7 @@ namespace CardGame {
         }
 
         public string getHandString() {
+            // Get the value and suit of each card delimited by commas and the rank of the hand.
             if (hand.getCount() < 5) {
                 throw new IndexOutOfRangeException();
             }
@@ -27,6 +28,35 @@ namespace CardGame {
                 handString += hand.at(i).cardValue + "-" + hand.at(i).cardSuit + ", ";
             }
             handString += hand.at(4).cardValue + "-" + hand.at(4).cardSuit;
+
+            if (hand.isFourOfAKind) {
+                handString += " - Four of a kind";
+            }
+            else if (hand.isStraight && hand.isFlush) {
+                handString += " - Straight flush";
+            }
+            else if (hand.isFlush) {
+                handString += " - Flush";
+            }
+            else if (hand.isStraight) {
+                handString += " - Straight";
+            }
+            else if (hand.isFullHouse) {
+                handString += " - Full House";
+            }
+            else if (hand.isThreeOfAKind) {
+                handString += " - Three of a Kind";
+            }
+            else if (hand.isTwoPair) {
+                handString += " - Two Pair";
+            }
+            else if (hand.isOnePair) {
+                handString += " - One Pair";
+            }
+            else {
+                handString += " - High card";
+            }
+
             return handString;
         }
     }
